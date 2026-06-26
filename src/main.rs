3fc651f3d91780636bcb61ffdf132d24063022d9
Jaxon Adams <jadamsresume1@gmail.com>
2026-06-25 22:48:44 -0600

    main: Add initial data model for `Book`

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
 use rusqlite::{Connection, Result};
 use std::{env, fs, path::PathBuf};
 
+mod model;
+
+use model::Book;
+
 const APP_NAME: &str = "bookworm";
 
 fn main() {
     println!("Hello, world!");
+
+    let test_book = Book {
+        id: 1,
+        title: String::from("Les Miserables"),
+        author: String::from("Victor Hugo"),
+        num_pages: 1218,
+    };
+
+    println!("Test Book: {:?}", test_book);
+
     let _connection = init_db();
 }
 
---
+++ src/model/book.rs
@@ -0,0 +1,7 @@
+#[derive(Debug)]
+pub struct Book {
+    pub id: i32,
+    pub title: String,
+    pub author: String,
+    pub num_pages: i32,
+}
